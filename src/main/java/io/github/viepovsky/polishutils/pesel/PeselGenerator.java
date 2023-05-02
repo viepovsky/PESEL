e32a95b45a14b32ef4dea94d2888cf08cba2192a
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-02 13:04:34 +0200

    Add methods for generating pesel in PeselGenerator class

--- src/main/java/io/github/viepovsky/polishutils/pesel/PeselGenerator.java
+++ src/main/java/io/github/viepovsky/polishutils/pesel/PeselGenerator.java
@@ -1,10 +1,108 @@
 package io.github.viepovsky.polishutils.pesel;
 
 import java.time.LocalDate;
+import java.util.Random;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class PeselGenerator {
-    private static final LocalDate MIN_DATE = LocalDate.of(1800,1,1);
-    private static final LocalDate MAX_DATE = LocalDate.of(2299, 12, 31);
+    private static final int[] CONTROL_WEIGHTS = new int[]{1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+    private static final LocalDate DEFAULT_MIN_DATE = LocalDate.now().minusYears(100);
+    private static final LocalDate DEFAULT_MAX_DATE = LocalDate.now();
+    private static final Random RANDOM = new Random();
+    private Pesel.Gender gender;
+    private LocalDate minDate;
+    private LocalDate maxDate;
 
+    public PeselGenerator(PeselGeneratorParams params) {
+        gender = params.getGender();
+        validateAndChangeInputGender();
+        minDate = params.getMinDate() != null ? params.getMinDate() : DEFAULT_MIN_DATE;
+        maxDate = params.getMaxDate() != null ? params.getMaxDate() : DEFAULT_MAX_DATE;
+        validateAndChangeInputDates();
+    }
 
+    private void validateAndChangeInputGender() {
+        if (gender == null) {
+            gender = RANDOM.nextInt(10) % 2 == 0 ? Pesel.Gender.FEMALE : Pesel.Gender.MALE;
+        }
+    }
+
+    private void validateAndChangeInputDates() {
+        if (minDate.isAfter(maxDate)) {
+            LocalDate tempDate = minDate;
+            minDate = maxDate;
+            maxDate = tempDate;
+        }
+    }
+
+    public String generatePesel() {
+        String birthDateDigits = getBirthDateDigit();
+        String randomSerialDigits = getRandomSerialDigits();
+        String genderDigit = getRandomGenderDigit();
+        String controlDigit = getControlDigit(birthDateDigits, randomSerialDigits, genderDigit);
+        return birthDateDigits + randomSerialDigits + genderDigit + controlDigit;
+    }
+
+    private String getBirthDateDigit() {
+        LocalDate birthDate = getRandomBirthDate(minDate, maxDate);
+        return encodeBirthDate(birthDate);
+    }
+
+    String encodeBirthDate(LocalDate birthDate) {
+        int year = birthDate.getYear();
+        int month = birthDate.getMonthValue();
+        int day = birthDate.getDayOfMonth();
+
+        switch (year / 100) {
+            case 18 -> month += 80;
+            case 20 -> month += 20;
+            case 21 -> month += 40;
+            case 22 -> month += 60;
+        }
+
+        return String.valueOf(year % 100) + month + day;
+    }
+
+    private LocalDate getRandomBirthDate(LocalDate minDate, LocalDate maxDate) {
+        long days = DAYS.between(minDate, maxDate);
+        return minDate.plusDays(RANDOM.nextLong(days + 1));
+    }
+
+    private String getRandomSerialDigits() {
+        return String.valueOf(RANDOM.nextInt(10)) + RANDOM.nextInt(10) + RANDOM.nextInt(10);
+    }
+
+    private String getRandomGenderDigit() {
+        if (gender.equals(Pesel.Gender.FEMALE)) {
+            return encodeRandomFemaleDigit();
+        } else {
+            return encodeRandomMaleDigit();
+        }
+    }
+
+    private String encodeRandomFemaleDigit() {
+        return String.valueOf(RANDOM.nextInt(5) * 2);
+    }
+
+    private String encodeRandomMaleDigit() {
+        return String.valueOf(RANDOM.nextInt(5) * 2 + 1);
+    }
+
+    private String getControlDigit(String birthDateDigits, String randomSerialDigits, String genderDigit) {
+        String pesel = birthDateDigits + randomSerialDigits + genderDigit;
+        int controlSum = 0;
+        for (int i = 0; i < 10; i++) {
+            int multipliedNumber = CONTROL_WEIGHTS[i] * Character.getNumericValue(pesel.charAt(i));
+            if (multipliedNumber >= 10) {
+                controlSum += Character.getNumericValue(String.valueOf(multipliedNumber).charAt(1));
+            } else {
+                controlSum += multipliedNumber;
+            }
+        }
+        if (controlSum >= 10) {
+            return String.valueOf((10 - Character.getNumericValue(String.valueOf(controlSum).charAt(1))));
+        }
+        return String.valueOf(10 - controlSum);
+    }
 }
