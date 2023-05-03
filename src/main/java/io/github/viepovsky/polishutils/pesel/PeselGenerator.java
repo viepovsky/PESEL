78cbbf9371eb16e0639039681921a27244cd39b9
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-03 13:48:15 +0200

    Add Javadoc to PeselGenerator class

--- src/main/java/io/github/viepovsky/polishutils/pesel/PeselGenerator.java
+++ src/main/java/io/github/viepovsky/polishutils/pesel/PeselGenerator.java
@@ -5,29 +5,101 @@ import java.util.Random;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
+/**
+ * The {@code PeselGenerator} class is used to generate random Polish Personal Identification Numbers (PESEL).
+ * <p>
+ * You can generate a PESEL using the static method {@code generatePeselStatic()} which uses the default
+ * parameters defined in the {@code PeselGeneratorParams} class. However, if you want to generate a PESEL with specific
+ * parameters, you can use the {@code PeselGeneratorParams} class to specify the desired gender and date range.
+ * <p>
+ * Example usage of the {@code PeselGenerator} class:
+ * <blockquote><pre>
+ *     String generatedPesel = PeselGenerator.generatePeselStatic(); // generates random PESEL with default params
+ * </pre></blockquote>
+ * or with usage of {@code PeselGeneratorParams} class:
+ * <blockquote><pre>
+ *     LocalDate minDate = LocalDate.of(1990, 1, 1);
+ *     LocalDate maxDate = LocalDate.of(2010, 1, 1);
+ *     PeselGeneratorParams.Gender gender = PeselGeneratorParams.Gender.FEMALE;
+ *
+ *     PeselGeneratorParams params = PeselGeneratorParams.builder()
+ *              .gender(gender)
+ *              .minDate(minDate)
+ *              .maxDate(maxDate)
+ *              .build();
+ *
+ *     PeselGenerator peselGenerator = new PeselGenerator(params);
+ *     String generatedPesel = peselGenerator.generatePesel();
+ *     //generates random PESEL for a female person born between January 1, 1990 and January 1, 2010
+ * </pre></blockquote>
+ * Passing a {@code null} argument to a constructor will cause a {@link NullPointerException} to be thrown.
+ *
+ * @author Oskar Rajzner
+ */
 public class PeselGenerator {
+    /**
+     * Digits weights for calculating the control digit of the PESEL.
+     */
     private static final int[] CONTROL_WEIGHTS = new int[]{1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+    /**
+     * A random number generator used for generating PESEL numbers.
+     */
     private static final Random RANDOM = new Random();
+    /**
+     * The gender that will be included in the generated PESEL numbers.
+     */
     private static PeselGeneratorParams.Gender gender;
+    /**
+     * The minimum date range from which a PESEL number will be generated.
+     */
     private LocalDate minDate;
+    /**
+     * The maximum date range to which a PESEL number will be generated.
+     */
     private LocalDate maxDate;
 
+    /**
+     * Creates a new instance of the {@code PeselGenerator} class with the specified {@code PeselGeneratorParams}.
+     *
+     * @param params the {@code PeselGeneratorParams} object that specifies the parameters for generating the PESEL numbers.
+     */
     public PeselGenerator(PeselGeneratorParams params) {
         gender = params.getGender();
         minDate = params.getMinDate();
         maxDate = params.getMaxDate();
     }
 
+    /**
+     * Generates a random PESEL number with default parameters.
+     * <p>
+     * The default parameters are as follows:
+     * <li>Random gender</li>
+     * <li>Date of birth between today's date and 100 years before today's date.</li>
+     *
+     * @return a randomly generated PESEL number as a string value
+     */
     public static String generatePeselStatic() {
         LocalDate minDate = PeselGeneratorParams.getDefaultMinDate();
         LocalDate maxDate = PeselGeneratorParams.getDefaultMaxDate();
         return generatePesel(minDate, maxDate);
     }
 
+    /**
+     * Generates a random PESEL number with given parameters passed in {@code PeselGeneratorParams} object.
+     *
+     * @return a randomly generated PESEL number as a string value
+     */
     public String generatePesel() {
         return generatePesel(minDate, maxDate);
     }
 
+    /**
+     * Main method to generate PESEL, it gathers all parts of PESEL and returns whole PESEL as result.
+     *
+     * @param minDate class field as LocalDate object
+     * @param maxDate class field as LocalDate object
+     * @return the PESEL number as String
+     */
     private static String generatePesel(LocalDate minDate, LocalDate maxDate) {
         String birthDateDigits = getBirthDateDigit(minDate, maxDate);
         String randomSerialDigits = getRandomSerialDigits();
@@ -36,11 +108,36 @@ public class PeselGenerator {
         return birthDateDigits + randomSerialDigits + genderDigit + controlDigit;
     }
 
+    /**
+     * Method for getting a random birth date within the specified date range and encoding it to match as PESEL part.
+     *
+     * @param minDate the minimum date range as a LocalDate object
+     * @param maxDate the maximum date range as a LocalDate object
+     * @return the encoded PESEL birth date as a String
+     */
     private static String getBirthDateDigit(LocalDate minDate, LocalDate maxDate) {
         LocalDate birthDate = getRandomBirthDate(minDate, maxDate);
         return encodeBirthDate(birthDate);
     }
 
+    /**
+     * Returns a random birth date within the specified date range
+     *
+     * @param minDate the minimum date range as a LocalDate object
+     * @param maxDate the maximum date range as a LocalDate object
+     * @return a random birth date within the specified range as a LocalDate object
+     */
+    private static LocalDate getRandomBirthDate(LocalDate minDate, LocalDate maxDate) {
+        long days = DAYS.between(minDate, maxDate);
+        return minDate.plusDays(RANDOM.nextLong(days + 1));
+    }
+
+    /**
+     * Encodes a birth date as a PESEL birth date part.
+     *
+     * @param birthDate the date of birth as a LocalDate object
+     * @return the encoded PESEL birth date as a String
+     */
     private static String encodeBirthDate(LocalDate birthDate) {
         int year = birthDate.getYear();
         int month = birthDate.getMonthValue();
@@ -72,15 +169,23 @@ public class PeselGenerator {
         return encodedBirthDate;
     }
 
-    private static LocalDate getRandomBirthDate(LocalDate minDate, LocalDate maxDate) {
-        long days = DAYS.between(minDate, maxDate);
-        return minDate.plusDays(RANDOM.nextLong(days + 1));
-    }
-
+    /**
+     * Generates a random string of three digits to represent the serial number in the PESEL number.
+     *
+     * @return a randomly generated three-digit string
+     */
     private static String getRandomSerialDigits() {
         return String.valueOf(RANDOM.nextInt(10)) + RANDOM.nextInt(10) + RANDOM.nextInt(10);
     }
 
+
+    /**
+     * Generates a random digit to represent the gender in the PESEL number. If a gender is specified,
+     * the PESEL will be generated with that gender. If no gender is specified, the generated gender will be random.
+     * Once a gender is generated, it is set to null so that it will be randomized on the next call to this method.
+     *
+     * @return the generated gender digit of the PESEL number as a string
+     */
     private static String getGenderRandomDigit() {
         if (!isGenderGiven()) {
             setRandomGender();
@@ -91,6 +196,27 @@ public class PeselGenerator {
         return checkGenderAndEncodeDigit();
     }
 
+    /**
+     * Checks if the gender was specified as a parameter.
+     *
+     * @return true if gender was specified, false otherwise
+     */
+    private static boolean isGenderGiven() {
+        return gender != null;
+    }
+
+    /**
+     * Sets a random gender to the gender field variable.
+     */
+    private static void setRandomGender() {
+        gender = RANDOM.nextInt(10) % 2 == 0 ? PeselGeneratorParams.Gender.FEMALE : PeselGeneratorParams.Gender.MALE;
+    }
+
+    /**
+     * Checks the gender and calls the appropriate method to generate a random gender digit.
+     *
+     * @return the generated gender digit of the PESEL number as a string
+     */
     private static String checkGenderAndEncodeDigit() {
         if (gender.equals(PeselGeneratorParams.Gender.FEMALE)) {
             return encodeRandomFemaleDigit();
@@ -99,22 +225,32 @@ public class PeselGenerator {
         }
     }
 
-    private static boolean isGenderGiven() {
-        return gender != null;
-    }
-
-    private static void setRandomGender() {
-        gender = RANDOM.nextInt(10) % 2 == 0 ? PeselGeneratorParams.Gender.FEMALE : PeselGeneratorParams.Gender.MALE;
-    }
-
+    /**
+     * Encodes a random female digit.
+     *
+     * @return the encoded random female digit as a String
+     */
     private static String encodeRandomFemaleDigit() {
         return String.valueOf(RANDOM.nextInt(5) * 2);
     }
 
+    /**
+     * Encodes a random male digit.
+     *
+     * @return the encoded random male digit as a String
+     */
     private static String encodeRandomMaleDigit() {
         return String.valueOf(RANDOM.nextInt(5) * 2 + 1);
     }
 
+    /**
+     * Calculates and returns the correct control digit for a generated PESEL number.
+     *
+     * @param birthDateDigits the birth date digits of the PESEL as a string
+     * @param randomSerialDigits the random serial digits of the PESEL as a string
+     * @param genderDigit the gender digit of the PESEL as a string
+     * @return the calculated control digit of the PESEL as a string
+     */
     private static String getControlDigit(String birthDateDigits, String randomSerialDigits, String genderDigit) {
         String pesel = birthDateDigits + randomSerialDigits + genderDigit;
         int controlSum = 0;
